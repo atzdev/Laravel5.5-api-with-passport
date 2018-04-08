35950f1f9015348af937377877c7667af3d37ecb
mookemjung <artzpluz@gmail.com>
2018-04-08 17:06:16 +0700

    Laravel5.5 rest-api with passport

---
+++ app/Http/Controllers/PassportController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facedes\Auth;
+use Validator;
+
+class PassportController extends Controller
+{
+    public $successStatus = 200;
+
+    public function login() {
+    	if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+    		$user->Auth::user();
+    		$success['token'] = $user->createToken('MyApp')->accessToken;
+    		return response()->json(['success' => $success], $this->successStatus);
+    	} else {
+    		return response()->json(['error' => 'Unauthorised'], 401);
+    	}
+    }
+
+    public function getDetails() {
+    	$user = Auth::user();
+    	return response()->json(['success' => $user], $this->successStatus);
+    }
+    
+
+
+
+
+
+
+
+}
